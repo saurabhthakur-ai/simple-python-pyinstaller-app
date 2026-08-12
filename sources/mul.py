3a8c07aad1e97689a7a10501b070803dbc96340a
srbhthkr <srbhthkr@gmail.com>
2026-08-12 09:45:25 +0530

    added multiplication file

---
+++ sources/mul.py
@@ -0,0 +1,15 @@
+
+
+
+def mul(a, b)
+    """
+    Multiplies two numbers.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The product of a and b.
+    """
+    return a * b
